6697ffd8601ef0e89e11d5cae67d8e1c33ca1331
dragneel <sayakascent@gmail.com>
2021-07-28 17:48:39 +0530

    commit

--- logincheck.php
+++ logincheck.php
@@ -35,14 +35,14 @@ if (isset($_POST['uname']) && isset($_POST['password'])) {
             	$_SESSION['user_name'] = $row['user_name'];
             	$_SESSION['name'] = $row['name'];
             	$_SESSION['id'] = $row['id'];
-            	header("Location: home.php");
+            	header("Location:home.php");
 		        exit();
             }else{
-				header("Location: index.php?error=Incorect User name or password");
+				header("Location:login.php?error=Incorect User name or password");
 		        exit();
 			}
 		}else{
-			header("Location: index.php?error=Incorect User name or password");
+			header("Location:login.php?error=Incorect User name or password");
 	        exit();
 		}
 	}
